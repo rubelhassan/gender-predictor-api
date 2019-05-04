37d1a649d138a6d359c471f6172aaabaeb1b70fc
Rubel Hassan <rubelhassan@outlook.com>
2019-05-04 22:09:54 +0600

    Added predict name endpoint
    /predict [POST] with request param [name] will give predicted gender with the name in return.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return 'App is up!'
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg': 'Welcome to Gender Predictor Api'})
+
+
+@app.route('/predict', methods=['POST'])
+def predict_gender():
+    name = request.json['name']
+    # TODO:: prediction task here
+    return jsonify({'name': name, 'gender': "Unknown"})
 
 
 if __name__ == '__main__':
